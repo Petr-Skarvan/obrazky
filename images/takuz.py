54ace4745bfffc8f1a9ec202998985e86cf3cfaa
Petr Škarvan <petrskarvan@seznam.cz>
2025-12-12 08:40:49 +0100

    Add files via upload

---
+++ images/takuz.py
@@ -0,0 +1,9 @@
+pocet_samohlasek = "aab"
+a = pocet_samohlasek.count("a")
+e = pocet_samohlasek.count("e")
+i = pocet_samohlasek.count("i")
+o = pocet_samohlasek.count("o")
+u = pocet_samohlasek.count("u")
+y = pocet_samohlasek.count("y")
+
+print(a+e+i+o+u+y)
